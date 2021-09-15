1894e7f7a1d9d4a536dd563c29f83c09a580ebde
Miroslav Demek <xdemek@fi.muni.cz>
2021-09-15 22:06:56 +0200

    Make http methods async

--- diskordpie/http.py
+++ diskordpie/http.py
@@ -4,11 +4,11 @@ class HttpClient:
     def __init__(self) -> None:
         pass
 
-    def post(self, url, data):
+    async def post(self, url, data):
         self.send_request("POST", url, data)
 
-    def get(self, url, data):
+    async def get(self, url, data):
         self.send_request("GET", url, data)
 
-    def send_request(method: str, url: str, data: str):
+    async def send_request(method: str, url: str, data: str):
         pass
